4b05555e88936a57a277ee46473d6bcf4b0201cb
khajornyot khamkhoon <khajornyot.kha@dplusonline.net>
2020-02-27 01:01:07 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {View, Text, Image, StyleSheet} from 'react-native';
+import Header from './components/Header';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Header />
+      {/* <Text style={styles.text}>Hello World!</Text>
+      <Image
+        source={{uri: 'https://randomuser.me/api/portraits/men/1.jpg'}}
+        style={styles.img}
+      /> */}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 60,
+  },
+  // text: {
+  //   color: 'darkslateblue',
+  //   fontSize: 30,
+  // },
+  // img: {
+  //   width: 100,
+  //   height: 100,
+  //   borderRadius: 100 / 2,
+  // },
+});
+
+export default App;
