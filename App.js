7f02d4bbdba6705aab50e8d2765103e0e2351e87
benzkjy <benzpos@gmail.com>
2020-02-27 01:18:55 +0700

    useState

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import {View, Text, Image, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
 import Header from './components/Header';
+import {uuid} from 'uuidv4';
 
 const App = () => {
+  const [items, setItems] = useState([
+    {id: uuid(), text: 'Milk'},
+    {id: uuid(), text: 'Eggs'},
+    {id: uuid(), text: 'Bread'},
+    {id: uuid(), text: 'Juice'},
+  ]);
+
   return (
     <View style={styles.container}>
       <Header />
-      {/* <Text style={styles.text}>Hello World!</Text>
-      <Image
-        source={{uri: 'https://randomuser.me/api/portraits/men/1.jpg'}}
-        style={styles.img}
-      /> */}
+      <FlatList
+        data={items}
+        renderItem={({item}) => <Text>{item.text}</Text>}
+      />
     </View>
   );
 };
